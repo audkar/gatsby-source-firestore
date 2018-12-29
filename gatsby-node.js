6e833525cd82ef5ecd69e3ca6c415fb45beb8f8c
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2018-12-29 18:18:26 +0200

    Offline mode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,10 @@
 const report = require('gatsby-cli/lib/reporter');
 const firebase = require('firebase-admin');
 const crypto = require('crypto');
+const fs = require('fs');
+const readline = require('readline');
+
+const offlineCacheFile = '.cache/offline-firestore.txt';
 
 const getDigest = id =>
   crypto
@@ -8,10 +12,7 @@ const getDigest = id =>
     .update(id)
     .digest('hex');
 
-exports.sourceNodes = async (
-  { boundActionCreators },
-  { types, credential }
-) => {
+async function loadFromFirestore(boundActionCreators, types, credential) {
   try {
     if (firebase.apps || !firebase.apps.length) {
       firebase.initializeApp({
@@ -32,26 +33,78 @@ exports.sourceNodes = async (
 
   const { createNode } = boundActionCreators;
 
+  if (fs.existsSync(offlineCacheFile)) {
+    fs.unlinkSync(offlineCacheFile);
+  }
+
   const promises = types.map(
     async ({ collection, type, map = node => node }) => {
       const snapshot = await db.collection(collection).get();
       for (let doc of snapshot.docs) {
         const contentDigest = getDigest(doc.id);
-        createNode(
-          Object.assign({}, map(doc.data()), {
-            id: doc.id,
-            parent: null,
-            children: [],
-            internal: {
-              type,
-              contentDigest,
-            },
-          })
-        );
+        const docData = Object.assign({}, map(doc.data()), {
+          id: doc.id,
+          parent: null,
+          children: [],
+          internal: {
+            type,
+            contentDigest,
+          },
+        });
+
+        fs.appendFileSync(offlineCacheFile, JSON.stringify(docData) + '\n');
+
+        const node = createNode(docData);
+
         Promise.resolve();
       }
     }
   );
   await Promise.all(promises);
+}
+
+async function loadFromFile(boundActionCreators, types) {
+  const { createNode } = boundActionCreators;
+
+  const lines = [];
+  const rl = readline.createInterface({
+    input: fs.createReadStream(offlineCacheFile),
+    terminal: false,
+  });
+  for await (const line of rl) {
+    lines.push(line);
+  }
+
+  const promises = types.map(
+    async ({ collection, type, map = node => node }, index) => {
+      //TODO offline mode currently supports only single collection
+      // should persist data with collection name
+      // const snapshot = await db.collection(collection).get();
+      for (let line of lines) {
+        const docData = JSON.parse(line);
+        const node = createNode(docData);
+        Promise.resolve();
+      }
+    }
+  );
+  await Promise.all(promises);
+}
+
+exports.sourceNodes = async (
+  { boundActionCreators },
+  { types, credential }
+) => {
+  const offlineMode = process.env.OFFLINE_FIRESTORE;
+  if (offlineMode) {
+    report.warn('Firestore source offline mode is ON!!!');
+  }
+  const useOfflineFile = offlineMode && fs.existsSync(offlineCacheFile);
+
+  if (useOfflineFile) {
+    await loadFromFile(boundActionCreators, types);
+  } else {
+    await loadFromFirestore(boundActionCreators, types, credential);
+  }
+
   return;
 };
